69a26e17f79b3116501034ad951622dd8165e6c5
GKOZAK200 <GKOZAK200@caledonian.ac.uk>
2023-03-12 18:03:48 +0000

    added template for homepage

--- honours_project/media_app/views.py
+++ honours_project/media_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This is the home page")
+    template = loader.get_template('homepage.html')
+    return HttpResponse(template.render())
