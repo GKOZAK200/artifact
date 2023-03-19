5cf6d15d067793d00b6ead50ba4d55c5e3f004c5
GKOZAK200 <GKOZAK200@caledonian.ac.uk>
2023-03-19 15:26:53 +0000

    implemented login page (not sure if it works)

--- honours_project/media_app/views.py
+++ honours_project/media_app/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
 def home(request):
     template = loader.get_template('homepage.html')
     return HttpResponse(template.render())
+
+def login_view(request):
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('')
+            else:
+                form.add_error(None, 'Invalid username or password')
+        else:
+            form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form })
