0e4a1062ab849a621083f3760e34d1026df75811
GKOZAK200 <GKOZAK200@caledonian.ac.uk>
2023-03-22 17:49:08 +0000

    Created media model

--- honours_project/media_app/admin.py
+++ honours_project/media_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Media
 
 # Register your models here.
+
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'media_type', 'release_year', 'poster_url')
+    list_filter = ('title', 'media_type', 'release_year')
+    search_fields = ('title', 'media_type', 'release_year')
+
+admin.site.register(Media, MediaAdmin)
--- honours_project/media_app/models.py
+++ honours_project/media_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Media(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    media_type = models.CharField(max_length=50)
+    release_year = models.IntegerField(blank=True)
+    poster_url = models.URLField(blank=True)
